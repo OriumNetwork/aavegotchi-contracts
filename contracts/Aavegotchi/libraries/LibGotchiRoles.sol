b86284be481f74e92a49bbcd610361fb3a8b30c6
Daniel Lima <daniel.lima@orium.network>
2023-10-18 13:13:51 -0300

    fix channeling permission check

--- contracts/Aavegotchi/libraries/LibGotchiRoles.sol
+++ contracts/Aavegotchi/libraries/LibGotchiRoles.sol
@@ -2,11 +2,16 @@
 pragma solidity 0.8.1;
 
 import {LibAppStorage, AppStorage} from "./LibAppStorage.sol";
-import {IERC7432} from "../../shared/interfaces/IERC7432.sol";
+import {IERC7432, RoleData} from "../../shared/interfaces/IERC7432.sol";
 
 library LibGotchiRoles {
     bytes32 public constant GOTCHIVERSE_PLAYER = keccak256("GOTCHIVERSE_PLAYER");
-    bytes32 public constant CHANNELING_ROLE = keccak256("CHANNELING_ROLE");
+
+    struct ProfitSplit {
+        uint256 lender;
+        uint256 borrower;
+        uint256 thirdParty;
+    }
 
     function isAavegotchiLent(uint32 _gotchiId) internal view returns (bool) {
         AppStorage storage s = LibAppStorage.diamondStorage();
@@ -17,22 +22,34 @@ library LibGotchiRoles {
 
     function hasGotchiversePlayerRole(uint32 _gotchiId, address _player) public view returns (bool) {
         AppStorage storage s = LibAppStorage.diamondStorage();
-        return IERC7432(s.rolesRegistry).hasRole(GOTCHIVERSE_PLAYER, address(this), _gotchiId, address(0), _player);
+        return IERC7432(s.rolesRegistry).hasRole(GOTCHIVERSE_PLAYER, address(this), _gotchiId, s.aavegotchis[_gotchiId].owner, _player);
     }
 
-    function rentalHasChannelingPermission(uint32 _gotchiId) public view returns (bool) {
-        address _lastGrantee = IERC7432(LibAppStorage.diamondStorage().rolesRegistry).lastGrantee(
+    function rentalHasChannelingPermission(uint32 _gotchiId) public returns (bool) {
+        AppStorage storage s = LibAppStorage.diamondStorage();
+        address _lastGrantee = IERC7432(s.rolesRegistry).lastGrantee(GOTCHIVERSE_PLAYER, address(this), _gotchiId, s.aavegotchis[_gotchiId].owner);
+
+        RoleData memory _roleData = IERC7432(s.rolesRegistry).roleData(
             GOTCHIVERSE_PLAYER,
             address(this),
             _gotchiId,
-            LibAppStorage.diamondStorage().aavegotchis[_gotchiId].owner
+            s.aavegotchis[_gotchiId].owner,
+            _lastGrantee
         );
 
-        return hasChannelingRole(_gotchiId, _lastGrantee);
+        // Checking if the roleData is valid
+        (bool success, ) = address(this).call(abi.encodeWithSignature("decodeData(bytes)", _roleData.data));
+
+        if (!success) {
+            (bool canChannelAlchemica, , ) = decodeData(_roleData.data);
+            return canChannelAlchemica;
+        } else {
+            // If the roleData is invalid, we assume that false is the default
+            return false;
+        }
     }
 
-    function hasChannelingRole(uint32 _gotchiId, address _player) public view returns (bool) {
-        AppStorage storage s = LibAppStorage.diamondStorage();
-        return IERC7432(s.rolesRegistry).hasRole(CHANNELING_ROLE, address(this), _gotchiId, address(0), _player);
+    function decodeData(bytes memory _data) public pure returns (bool, ProfitSplit memory, address) {
+        return abi.decode(_data, (bool, ProfitSplit, address));
     }
 }
